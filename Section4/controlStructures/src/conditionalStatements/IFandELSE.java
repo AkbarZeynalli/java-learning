26d04ea00746a2cf621945aae0608642bfe0d9c5
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-03 18:42:12 +0400

    java da if else ye giriş etdim və bir iki ədəd misal həll etdim giriş olaraq

---
+++ Section4/controlStructures/src/conditionalStatements/IFandELSE.java
@@ -0,0 +1,80 @@
+package conditionalStatements;
+
+import java.util.Scanner;
+
+public class IFandELSE {
+
+////        Sual1:İki ədəd arasında maksimumu tapmaq
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("1 ci ededi daxil edin: ");
+//        int firstNumber  = scanner.nextInt();
+//        System.out.print("2 ci ededi daxil edin: ");
+//        int secondNumber  = scanner.nextInt();
+//        System.out.println("Daxil edilən rəqəmlər: " + firstNumber +" | "+ secondNumber);
+//        int maxnumber;
+//        if (firstNumber > secondNumber) {
+//            maxnumber = firstNumber;
+//            System.out.println("Maksimum:" + maxnumber);
+//        } else if (firstNumber == secondNumber) {
+//            maxnumber = firstNumber;
+//            System.out.println("daxil edilən hər iki ədəd bir birinə bərabərdir." + maxnumber);
+//        }else{
+//            maxnumber =secondNumber;
+//            System.out.println("ikinci ədəd ən böyükdür."+maxnumber);
+//    }
+
+
+//2)Üç ədəd arasında maksimumu tapmaq
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("1 ci reqemi daxil et: ");
+//        int firstNumber = scanner.nextInt();
+//        System.out.print("2 ci reqemi daxil et: ");
+//        int secondNumber = scanner.nextInt();
+//        System.out.print("3 cü reqemi daxil et: ");
+//        int threeNumber = scanner.nextInt();
+//        System.out.println("Daxil edilən rəqəmlər: "+ firstNumber + " | "+secondNumber+ " | "+threeNumber);
+//        int maxNumber;
+//        if (firstNumber >secondNumber){
+//            if (firstNumber>threeNumber){
+//                maxNumber = firstNumber;
+//                System.out.println("Daxil edilən rəqəmlər arasında ən böyük rəqəm 1 ci reqəmdi: "+firstNumber);
+//            }
+//            else{
+//                maxNumber = threeNumber;
+//                System.out.println("Daxil edilən rəqəmlər arasında ən böyük rəqəm 3 cü reqəmdi: "+threeNumber);
+//            }
+//        }
+//        else {
+//            if (secondNumber > threeNumber){
+//                maxNumber = secondNumber;
+//                System.out.println("Daxil edilən rəqəmlər arasında ən böyük rəqəm 2 ci reqəmdi: "+secondNumber);
+//            }
+//            else {
+//                maxNumber = threeNumber;
+//                System.out.println("Daxil edilən rəqəmlər arasında ən böyük rəqəm 3 cü reqəmdi: "+threeNumber);
+//            }
+//        }
+//    }
+
+
+//    3)Ədədin müsbət, mənfi və ya sıfır olduğunu yoxlamaq üçün JAVA  proqramı
+//public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.print("Zəhmət olmasa bir rəqəm daxil edin: ");
+//    int inputNumber = scanner.nextInt();
+//    if (inputNumber<0){
+//        System.out.println("Sizin daxil etdiyiniz rəqəm mənfidir.");
+//    }
+//    else if (inputNumber == 0) {
+//        System.out.println("Daxil etdiyiniz rəqəm 0 a bərabərdir.");
+//    }
+//    else {
+//        System.out.println("Daxil etdiyiniz rəqəm müsbətdir.");
+//    }
+//}
+
+
+//    4)Ədədin 5-ə və 11-ə bölünüb-bölünmədiyini yoxlamaq üçün C proqramı
+}
