061366fac6a085de2820a3e426ec6a3cdd5f1bf9
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-04 23:37:40 +0400

    for a aid biraz misall həll etdim və əlavə edirəm

--- Section4/controlStructures/src/LoopingStatements/Forloop.java
+++ Section4/controlStructures/src/LoopingStatements/Forloop.java
@@ -1,9 +1,39 @@
 package LoopingStatements;
 
+import java.util.Scanner;
+
 public class Forloop {
     public static void main(String[] args) {
-        for (int i = 1;i<11;i++){
-            System.out.println(i+". Salam Mən Əkbər,Ləqəbim Lətif");
-        }
+//        for (int i = 1;i<11;i++){
+//            System.out.println(i+". Salam Mən Əkbər,Ləqəbim Lətif");
+//        }
+
+//        Sual1:İlk 10 Natural Rəqəm Ekranı
+//        for (int i = 1;i<=10;i++){
+//            System.out.print(i + " " );
+//        }
+
+
+//        2. İlk 10 natural ədədin cəmi
+//        System.out.println("İlk 10 natural ədəd: ");
+//        int i,sum = 0;
+//        for (i = 1;i<= 10;i++){
+//            System.out.print(i +" ");
+//            sum +=i;
+//        };
+//        System.out.println("\nCəmi: "+sum);
+
+
+//        3. Natural ədədlərin şərtlərini və onların cəmini göstərin
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Test Məlumatı: ");
+//        int inputNumber  = scanner.nextInt();
+//        System.out.println("İlk "+inputNumber+" natural ədəd :");
+//        int i, sum= 0;
+//        for (i = 1;i<= inputNumber;i++){
+//            System.out.print(i +" ");
+//            sum += i;
+//        }
+//        System.out.println("\n"+inputNumber +" şərtə qədər Natural ədədlərin cəmi :"+sum);
     }
 }
