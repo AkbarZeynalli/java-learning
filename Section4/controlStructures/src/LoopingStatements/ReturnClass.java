93849d50b5fb5e808fbddab89bbbd054eb57f872
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-06 23:21:42 +0400

    return e aid misal yazildi

---
+++ Section4/controlStructures/src/LoopingStatements/ReturnClass.java
@@ -0,0 +1,12 @@
+package LoopingStatements;
+
+public class ReturnClass {
+    public static void main(String[] args) {
+        for (int i = 0;i<5;i++){
+            if (i==4){
+                continue;
+            }
+            System.out.println("return i deyerleri: "+i);
+        }
+    }
+}
