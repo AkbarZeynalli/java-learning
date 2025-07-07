12eac9a0940668eafa6ff003dd288fa68dd397bc
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-07 21:11:03 +0400

    section 4 ün ikinci sualını yazdım çox asan deyildi çoxda çətin deyildi bitirdim

--- Section4-Questions/SectionfourQuestions/src/Questions2.java
+++ Section4-Questions/SectionfourQuestions/src/Questions2.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Questions2 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("                   Azərlotereya Açıq Səhmdar Cəmiyyətinə xoş gəlmisiniz");
+
+        System.out.print("Zəhmət olmasa iki rəqəmli ədəd daxil edin(XX):  ");
+        int inputNumber = scanner.nextInt();
+        int inputNumberFirstNumber = inputNumber/10;
+        int inputNumberSecondNumber = inputNumber % 10;
+
+        int randomNumber = (int)(Math.random()*100 +1);
+        int randomNumberFirstNumber = randomNumber /10;
+        int randomNumberSecondNumber = randomNumber %10;
+
+
+        System.out.println("Daxil edilən Rəqəm: "+inputNumber);
+        System.out.println("Verilən Random Rəqəm: "+randomNumber);
+
+
+        if (inputNumber ==randomNumber){
+            System.out.println("Təbriklər, Bizdən 10000AZN Pul qazandınız!");
+        } else if (inputNumberFirstNumber ==randomNumberSecondNumber && inputNumberSecondNumber ==randomNumberFirstNumber) {
+            System.out.println("Təbriklər,siz bizdən 5000AZN Pul qazandınız!");
+        } else if (inputNumberFirstNumber==randomNumberFirstNumber||inputNumberSecondNumber ==randomNumberSecondNumber) {
+            System.out.println("Təbriklər siz bizdən 1000AZN qazandınız");
+        }
+        else{
+            System.out.println("Şansınızı birdaha sınayın");
+        }
     }
 }
