c8d615201f1681462a84949acafd3bb0cc0dd5e8
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-07 13:15:28 +0400

    bu gün java da  Scanner i öyrəndim, hansı typde da hansı cür məlumat daxil edildiyini öyrəndim və bunlara aid məlumat daxil etdməyi praktika etdim

---
+++ Section4/controlStructures/src/Scanner/firstScannerClass.java
@@ -0,0 +1,34 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class firstScannerClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Rəqəm:
+//        System.out.print("Rəqəm daxil edin: ");
+//        int inputNumber = scanner.nextInt();
+//        System.out.println("Daxil edilən rəqəm: "+inputNumber);
+
+//        Mətn:
+//        System.out.print("Mətn daxil edin: ");
+//        String inputText = scanner.nextLine();
+//        System.out.println("Daxil edilen metn: "+inputText);
+
+//        double:
+//        System.out.print("Rəqəm daxil edin: ");
+//        double inputDoubleNumber = scanner.nextDouble();
+//        System.out.println("Daxil edilen Double Reqem: "+ inputDoubleNumber);
+
+//        next və nextLine ın fərqi:
+//        System.out.print("Char daxil edin: ");
+//        String inputChar = scanner.next();
+//        System.out.println("Daxil edilen Char: "+ inputChar);
+
+//        char:
+//        System.out.print("Char daxil edin: ");
+//        char inputCharVariable = scanner.next().charAt(0);
+//        System.out.println("Daxil edilen Char: "+ inputCharVariable);
+    }
+}
