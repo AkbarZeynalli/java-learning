5444dc76e4be2907d156b51d12fab71db5bd5f66
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-08 23:21:39 +0400

    section5 yaradıldı və ilk sualı həll etdim diyərlərini sabah edəcəm

---
+++ Section5/questionsFromTheFirst4Sections/src/Video30/Video30Questions.java
@@ -0,0 +1,20 @@
+package Video30;
+
+import java.util.Scanner;
+
+public class Video30Questions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Sual1:
+//        int sum = 0;
+//        for (int i = 1;i<=3;i++){
+//            System.out.print(i+" Reqemi daxil edin: ");
+//            sum += scanner.nextInt();
+//        }
+//        System.out.print("Daxil edilən ədədlərin ortalaması: "+(sum/3));
+
+//        Sual2:
+        
+    }
+}
