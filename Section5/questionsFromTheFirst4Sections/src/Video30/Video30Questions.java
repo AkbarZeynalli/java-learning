9486c7d04dc70734fca0e46767452ba8ddc451f7
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-09 17:01:48 +0400

    Section5 də suallar işlədim,6 sual işlədim

--- Section5/questionsFromTheFirst4Sections/src/Video30/Video30Questions.java
+++ Section5/questionsFromTheFirst4Sections/src/Video30/Video30Questions.java
@@ -14,7 +14,71 @@ public class Video30Questions {
 //        }
 //        System.out.print("Daxil edilən ədədlərin ortalaması: "+(sum/3));
 
-//        Sual2:
-        
+//        Sual2: klaviaturadan kənarları girilən bir üçbucağın növünü verən bir proqram yazın
+//        System.out.print("Üçbucağın 1. tərəfini daxil edin: ");
+//        float first = scanner.nextFloat();
+//        System.out.print("Üçbucağın 2. tərəfini daxil edin: ");
+//        float second = scanner.nextFloat();
+//        System.out.print("Üçbucağın 3. tərəfini daxil edin: ");
+//        float three = scanner.nextFloat();
+//        if (first == second && first ==three){
+//            System.out.println("Bu üçbucaq bərabərtərəfli üçbucaqdır.");
+//        } else if (first ==second ||first ==three ||second ==three) {
+//            System.out.println("Bu üçbucaq bərabəryanlı üçbucaqdır.");
+//        }else {
+//            System.out.println("Bu üçbucaq müxtəliftərəfli üçbucaqdır.");
+//        }
+
+
+//        Sual3:
+//        System.out.print("Vize notunu giriniz: ");
+//        double vize = scanner.nextDouble();
+//        System.out.print("Final notunu giriniz: ");
+//        double finalNotu = scanner.nextDouble();
+//        // Ortalama hesablanır
+//        double ortalama = (vize * 0.4) + (finalNotu * 0.6);
+//        // Nəticəyə baxılır
+//        System.out.println("Ortalama: " + ortalama);
+//        if (ortalama >= 50) {
+//            System.out.println("Tebrikler, dersi geçtiniz.");
+//        } else {
+//            System.out.println("Maalesef, dersten kaldınız.");
+//        }
+
+
+//        Sual4:Kendi adınızı ekrana 5 kere yazdıran uygulamayı tüm döngülerle yapınız.
+//        System.out.print("Adinizi daxil edin: ");
+//        String firstName = scanner.nextLine();
+//        for (int i = 1;i<=5;i++){
+//            System.out.println("for ilə Adınız: "+firstName);
+//        }
+//        int whileCount = 1;
+//        while (whileCount <=5){
+//            System.out.println("while ilə Adınız: "+firstName);
+//            whileCount++;
+//        }
+//        int doCount= 1;
+//        do {
+//            System.out.println("Do-While ilə Adınız: "+firstName);
+//            doCount++;
+//        }while (doCount<=5);
+
+
+//        Sual5:1’den 100’e kadar olan sayıların toplamını bulan uygulamayı yazınız.
+//        int sum= 0;
+//        for (int i = 1;i<=100;i++){
+//            sum+=i;
+//        }
+//        System.out.println("1’den 100’e kadar olan sayıların toplamı: "+sum);
+
+//        Sual6:1’den 10a kadar olan sayıları sıra ile ve aralarında virgül olacak şekilde yazan bir
+//        uygulamayı for döngüsü ile yazınız. 1,2,3,........ 10
+//        for (int i =1;i<=10;i++){
+//            System.out.print(i);
+//            if (i !=10){
+//                System.out.print(",");
+//                continue;
+//            }
+//        }
     }
 }
