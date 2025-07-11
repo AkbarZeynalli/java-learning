e5cee382c4ea7140ed839e64e5b35bf904278c9e
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-07-11 23:20:22 +0400

    section6 da methodlara girdim ilk derse baxdim sabah mesq edecem

---
+++ Section6/Java Methods/src/MethodsIntro.java
@@ -0,0 +1,8 @@
+public class MethodsIntro {
+    public static void main(String[] args) {
+        ReqemleriTopla();
+    }
+    public static void ReqemleriTopla(){
+        System.out.println("Salam Akbar");
+    }
+}
